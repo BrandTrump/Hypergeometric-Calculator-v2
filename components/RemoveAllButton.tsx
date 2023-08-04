0aea042bc268c6bfbae771d71b0558db02bf8902
Brandon Trump <brandontrump101@gmail.com>
2023-08-04 15:45:29 +1200

    feat: created a button to remove all selected cards inside of the probability list

---
+++ components/RemoveAllButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useCardStore } from "@/store/card-store";
+
+function RemoveAllButton() {
+  const { selectedCards, resetSelectedCardState } = useCardStore();
+
+  const handleRemoveAll = () => {
+    resetSelectedCardState();
+  };
+
+  return (
+    <button
+      onClick={handleRemoveAll}
+      disabled={selectedCards.length === 0}
+      className="bg-gray-300 border border-black rounded-md font-semibold hover:bg-gray-400 transition duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed px-3 py-1 text-black"
+    >
+      Remove All
+    </button>
+  );
+}
+
+export default RemoveAllButton;
