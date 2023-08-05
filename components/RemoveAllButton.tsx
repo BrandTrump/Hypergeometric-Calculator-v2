400fa9af850cad9013b10bcf11a0b4d68bd3f313
Brandon Trump <brandontrump101@gmail.com>
2023-08-05 20:24:01 +1200

    feat: reset state for amt, min, & max

--- components/RemoveAllButton.tsx
+++ components/RemoveAllButton.tsx
@@ -2,19 +2,28 @@
 import { useCardStore } from "@/store/card-store";
 
 function RemoveAllButton() {
-  const { selectedCards, resetSelectedCardState, resetCalculationState } =
-    useCardStore();
+  const {
+    selectedCards,
+    resetSelectedCardState,
+    resetCalculationState,
+    resetAmtState,
+    resetMinState,
+    resetMaxState,
+  } = useCardStore();
 
   const handleRemoveAll = () => {
     resetSelectedCardState();
     resetCalculationState();
+    resetAmtState();
+    resetMinState();
+    resetMaxState();
   };
 
   return (
     <button
       onClick={handleRemoveAll}
       disabled={selectedCards.length === 0}
-      className="bg-gray-300 border border-black rounded-md font-semibold hover:bg-gray-400 transition duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed px-3 py-1 text-black"
+      className="bg-gray-300 border border-black rounded-md font-semibold hover:bg-gray-400 transition duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed px-3 py-1 text-black w-full md:w-auto"
     >
       Remove All
     </button>
