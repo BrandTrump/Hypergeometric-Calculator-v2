3e21f9785d787df18bff844687bfe4506d54fa95
Brandon Trump <brandontrump101@gmail.com>
2023-10-07 00:51:21 +1300

    feat: added columns to display the card search, deck list, card groupings & calculation.

--- app/deck/[cardIds]/page.tsx
+++ app/deck/[cardIds]/page.tsx
@@ -1,6 +1,7 @@
 import CardGrid from "@/components/CardGrid";
 import CardSearch from "@/components/CardSearch";
 import Container from "@/components/Container";
+import DeckList from "@/components/DeckList";
 import { getCardData } from "@/helpers/getCardData";
 
 async function DeckPage({ params }: { params: { cardIds: string } }) {
@@ -11,7 +12,12 @@ async function DeckPage({ params }: { params: { cardIds: string } }) {
   return (
     <Container>
       <CardGrid cardData={cardData} deckSize={deckSize} cardIds={cardArr} />
-      <CardSearch />
+      <section className="grid grid-cols-4 h-[30em] border-orange-500 border-2 divide-orange-500 divide-x-2">
+        <CardSearch />
+        <DeckList cardData={cardData} />
+        <div className="bg-purple-600"></div>
+        <div className="bg-yellow-400"></div>
+      </section>
     </Container>
   );
 }
