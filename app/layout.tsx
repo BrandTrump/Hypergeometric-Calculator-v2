97032b6dd7341a5542afef6324fd78e30c0652f1
Brandon Trump <brandontrump101@gmail.com>
2024-01-20 23:32:51 +1300

    fix: added Toaster component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
