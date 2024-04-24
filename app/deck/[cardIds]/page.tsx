1a3e24d7a25c4c17510d722ebc2476a912faef17
Brandon Trump <brandontrump101@gmail.com>
2024-04-24 14:33:57 +1200

    feat: included deck size

--- app/deck/[cardIds]/page.tsx
+++ app/deck/[cardIds]/page.tsx
@@ -1,27 +1,29 @@
-"use client";
-import CardGrid from "@/components/CardGrid";
 import CardSearch from "@/components/CardSearch";
 import Container from "@/components/Container";
 import DeckList from "@/components/DeckList";
-import { getCardData } from "@/helpers/getCardData";
 import { Suspense } from "react";
 
 function DeckPage({ params }: { params: { cardIds: string } }) {
-  // const cardData = await getCardData(params.cardIds);
-  // const deckSize = params.cardIds.split("%2C").length;
-  // const cardArr = params.cardIds.split("%2C");
-
+  const deckSize = params.cardIds.split("%2C").length;
   return (
     <Container>
-      {/* <CardGrid cardData={cardData} deckSize={deckSize} cardIds={cardArr} /> */}
-      <section className="grid grid-cols-4 border-orange-500 border-2 divide-orange-500 divide-x-2 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90 mt-36 md:mt-32">
-        <CardSearch />
-        <Suspense fallback={<div>Loading...</div>}>
-          <DeckList cardIds={params.cardIds} />
-        </Suspense>
-        <div></div>
-        <div></div>
-      </section>
+      <main className="border border-gray-500 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90 mt-36 md:mt-32">
+        <div className="p-2 bg-gray-800 mb-2">
+          <h1 className="font-semibold text-white">Deck</h1>
+        </div>
+        <div className="flex items-center p-2 bg-gray-800">
+          <h2 className="font-semibold text-white">Main Deck</h2>
+          <p className="font-semibold text-white ml-12">{deckSize}</p>
+        </div>
+        <section className="grid grid-cols-4 border-orange-500 border-2 divide-orange-500 divide-x-2 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90">
+          <CardSearch />
+          <Suspense fallback={<div>Loading...</div>}>
+            <DeckList cardIds={params.cardIds} />
+          </Suspense>
+          <div></div>
+          <div></div>
+        </section>
+      </main>
     </Container>
   );
 }
