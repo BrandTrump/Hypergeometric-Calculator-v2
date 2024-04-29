d3c8afb0a4011bf05308377685753d2bc01c88ae
Brandon Trump <brandontrump101@gmail.com>
2024-04-29 15:27:40 +1200

    ref: refactored DeckPage component and added Board component for displaying card data.

--- app/deck/[cardIds]/page.tsx
+++ app/deck/[cardIds]/page.tsx
@@ -1,29 +1,12 @@
-"use client";
-import CardSearch from "@/components/CardSearch";
 import Container from "@/components/Container";
-import DeckList from "@/components/DeckList";
-import { useState } from "react";
-import { DndContext, DragEndEvent, UniqueIdentifier } from "@dnd-kit/core";
-import Draggable from "@/components/Draggable";
-import Droppable from "@/components/Droppable";
-import Card from "@/components/Card";
+import { Suspense } from "react";
+import { getCardData } from "@/helpers/getCardData";
+import Board from "@/components/Board";
 
-function DeckPage({ params }: { params: { cardIds: string } }) {
+async function DeckPage({ params }: { params: { cardIds: string } }) {
   const deckSize = params.cardIds.split("%2C").length;
 
-  const containers = ["A", "B"];
-  const [parent, setParent] = useState<UniqueIdentifier>("A");
-
-  const handleDragEnd = (e: DragEndEvent) => {
-    const { over } = e;
-
-    if (!over) {
-      return;
-    }
-    setParent(over.id);
-  };
-
-  const draggableMarkup = <Draggable id="draggable">Card Name</Draggable>;
+  const cardData = await getCardData(params.cardIds);
 
   return (
     <Container>
@@ -35,26 +18,9 @@ function DeckPage({ params }: { params: { cardIds: string } }) {
           <h2 className="font-semibold text-white">Main Deck</h2>
           <p className="font-semibold text-white ml-12">{deckSize}</p>
         </div>
-        {/* <section className="grid grid-cols-4 border-orange-500 border-2 divide-orange-500 divide-x-2 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90">
-          <CardSearch />
-          <Suspense fallback={<div>Loading...</div>}>
-            <DeckList cardIds={params.cardIds} />
-          </Suspense>
-          <div></div>
-          <div></div>
-        </section> */}
-        <DndContext onDragEnd={handleDragEnd}>
-          {parent === null ? draggableMarkup : null}
-          <div className="grid grid-cols-4 border-orange-500 border-2 divide-orange-500 divide-x-2 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90">
-            <CardSearch />
-            {containers.map((id) => (
-              <Droppable key={id} id={id}>
-                {parent === id ? draggableMarkup : "Drop here"}
-              </Droppable>
-            ))}
-            <div></div>
-          </div>
-        </DndContext>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Board cardData={cardData} />
+        </Suspense>
       </main>
     </Container>
   );
