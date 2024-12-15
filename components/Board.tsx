725eb1be852f0e281995c0df415e90c37e8e4eca
Brandon Trump <brandontrump101@gmail.com>
2024-12-15 17:08:26 +1300

    Add drag end event type definition

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,16 +1,7 @@
 "use client";
-import {
-  DndContext,
-  DragEndEvent,
-  DragOverlay,
-  DragStartEvent,
-  UniqueIdentifier,
-} from "@dnd-kit/core";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import CardSearch from "./CardSearch";
 import { useState } from "react";
-import Droppable from "./Droppable";
-import Card from "./Card";
-import Draggable from "./Draggable";
 import Column from "./Column";
 
 type BoardProps = {
@@ -44,29 +35,9 @@ export default function Board({ cardData }: BoardProps) {
   return (
     <div className="grid grid-cols-4 border-orange-500 border-2 divide-orange-500 divide-x-2 bg-gradient-to-r from-black/90 via-sky-900/90 to-black/90">
       <CardSearch />
-      {/* <div className="flex flex-col gap-2 p-2">
-          {cardData.data.map((card) => (
-            <Draggable key={card.id} id={card.id.toString()}>
-            <Card name={card.name} />
-            </Draggable>
-            ))}
-            </div>
-            
-            <DragOverlay>
-            {activeId ? <Card name={activeId.toString()} /> : "Drop Here"}
-            </DragOverlay> */}
 
       <DndContext onDragEnd={handleDragEnd} id="card-dnd">
         {containers.map((container) => (
-          // <Droppable key={container.id} id={container.id}>
-          //   <div className="flex flex-col gap-3 p-2">
-          //     {cards.map((card) => (
-          //       <Draggable key={card.id} id={JSON.stringify(card.id)}>
-          //         <Card name={card.name} />
-          //       </Draggable>
-          //     ))}
-          //   </div>
-          // </Droppable>
           <Column
             key={container.id}
             container={container}
